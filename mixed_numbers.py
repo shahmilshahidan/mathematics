eebeb0c434505341c50096ac9445610591fe9ff5
Shahmil Shahidan <shahmilshahidan@gmail.com>
2021-08-08 16:48:27 +0800

    Successfully cover all possible cases of mixed number.
    1. Proper fraction
    	a. Simplifiable fraction
    	b. Unsimplifiable fraction
    2. Improper fraction
    	a. Positive fraction or both containing negative values (-n/-d)
    	b. Negative fraction
    
    Signed off by: Muhammad Shahmil Shahidan

--- mixed_numbers.py
+++ mixed_numbers.py
@@ -1,4 +1,4 @@
-import re
+import re, math
 
 def mixed_fraction(s):
     parsing = re.split("\/", s)
@@ -12,33 +12,56 @@ def mixed_fraction(s):
     if result == 0:
         return str(numerator // denominator)
     else:
+        # All positive fractions or all negative fractions
         if (numerator > 0 and denominator > 0) or (numerator < 0 and denominator < 0):
-            # All positive and all negative
-            remaining = divmod(numerator, denominator)
-            whole_num = remaining[0]
-            fraction = simplify(remaining[1], denominator)
-            return mixed_number_format(whole_num, fraction[0], fraction[1])
+            proper_fraction = factoring(numerator, denominator)
+            return mixed_number_format(proper_fraction[0], proper_fraction[1], proper_fraction[2])
         else:
-            if numerator < 0:
-                remaining = divmod(abs(numerator), denominator)
-                whole_num = remaining[0]
-                fraction = simplify(remaining[1], denominator)
-                return mixed_number_format(-whole_num, fraction[0], fraction[1])
+            # Numerator is negative
+            # Improper fraction case
+            if numerator < 0 and (abs(numerator) > abs(denominator)):
+                proper_fraction = factoring(abs(numerator), denominator)
+                return mixed_number_format(-proper_fraction[0], proper_fraction[1], proper_fraction[2])
+            # Proper fraction case
+            elif numerator < 0 and (abs(numerator) < abs(denominator)):
+                # Quotient equals to numerator
+                if numerator // denominator == -1:
+                    pf = simplify(abs(numerator), denominator)
+                    return mixed_number_format(0, -pf[0], pf[1])
+                proper_fraction = factoring(abs(numerator), denominator)
+                return mixed_number_format(proper_fraction[0], -proper_fraction[1] * -1, proper_fraction[2])
+            # Denominator is negative
             elif denominator < 0:
-                remaining = divmod(numerator, abs(denominator))
-                whole_num = remaining[0]
-                fraction = simplify(remaining[1], denominator)
-                return mixed_number_format(-whole_num, fraction[0], fraction[1])
-        
+                # Quotient equals to numerator
+                if numerator // denominator == -1:
+                    pf = simplify(numerator, abs(denominator))
+                    return mixed_number_format(0, -pf[0], pf[1])
+                proper_fraction = factoring(numerator, abs(denominator))
+                return mixed_number_format(-proper_fraction[0], proper_fraction[1], proper_fraction[2])
+
+
+def proper_fraction(numerator, denominator):
+    # Quotient equals to numerator
+    if numerator // denominator == -1:
+        pf = simplify(abs(numerator), denominator)
+        return mixed_number_format(0, -pf[0], pf[1])
+    proper_fraction = factoring(abs(numerator), denominator)
+    return mixed_number_format(proper_fraction[0], -proper_fraction[1] * -1, proper_fraction[2])
+
+def improper_fraction(numerator, denominator):
+    proper_fraction = factoring(abs(numerator), denominator)
+    return mixed_number_format(-proper_fraction[0], proper_fraction[1], proper_fraction[2])
+
+def factoring(x,y):
+    remaining = divmod(x, y)
+    whole_num = remaining[0]
+    fraction = simplify(remaining[1], y)
+    return [whole_num, fraction[0], fraction[1]]
 
 def simplify(x, y):
-    i = 2
-    while i < min(x,y) + 1:
-        if x % i == 0 and y % i == 0:
-            x = x // i
-            y = y // i
-        else:
-            i += 1
+    divisor = math.gcd(x,y)
+    x = x // divisor
+    y = y // divisor
     if x < 0 and y < 0:
         x = x // -1
         y = y // -1
@@ -46,8 +69,8 @@ def simplify(x, y):
 
 def mixed_number_format(i, numerator, denominator):
     if i == 0:
-        return str(numerator) + '/' + str(denominator)    
+        return str(numerator) + '/' + str(denominator)
     return str(i) + ' ' + str(numerator) + '/' + str(denominator)
 
-print(mixed_fraction('42/9'))
-#print(-10%7)
+print(1104055 // 3033796)
+print(1104055 % -3033796)
