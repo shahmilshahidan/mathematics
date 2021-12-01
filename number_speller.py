d99f9774477f22ea1dd9294121def5388acebcb4
shahmilshahidan <shahmilshahidan@gmail.com>
2021-12-01 23:23:27 +0800

    Edited number_speller.py and added test cases in number_speller_test.py. WIP: Getting the spaces right.
    Signed off by: Muhammad Shahmil Shahidan<shahmilshahidan@gmail.com>

---
+++ number_speller.py
@@ -0,0 +1,194 @@
+"""
+Muhammad Shahmil Shahidan
+Program that spell a number requested.
+Number range is from 0 to 99999
+"""
+
+
+# Case 1: For single digits
+def ones(number):
+    if number == '0':
+        return 'zero'
+    elif number == '1':
+        return 'one'
+    elif number == '2':
+        return 'two'
+    elif number == '3':
+        return 'three'
+    elif number == '4':
+        return 'four'
+    elif number == '5':
+        return 'five'
+    elif number == '6':
+        return 'six'
+    elif number == '7':
+        return 'seven'
+    elif number == '8':
+        return 'eight'
+    elif number == '9':
+        return 'nine'
+
+# Case 2: For two digits(20+) and above
+def ones_mul(number):
+    if number == '0':
+        return ''
+    elif number == '1':
+        return 'one'
+    elif number == '2':
+        return 'two'
+    elif number == '3':
+        return 'three'
+    elif number == '4':
+        return 'four'
+    elif number == '5':
+        return 'five'
+    elif number == '6':
+        return 'six'
+    elif number == '7':
+        return 'seven'
+    elif number == '8':
+        return 'eight'
+    elif number == '9':
+        return 'nine'
+
+def tens(number):
+    if number[0] == '0':
+        return ''
+    if number[0] != '1':
+        if number[0] == '2':
+            return 'twenty'
+        elif number[0] == '3':
+            return 'thirty'
+        elif number[0] == '4':
+            return 'forty'
+        elif number[0] == '5':
+            return 'fifty'
+        elif number[0] == '6':
+            return 'sixty'
+        elif number[0] == '7':
+            return 'seventy'
+        elif number[0] == '8':
+            return 'eighty'
+        elif number[0] == '9':
+            return 'ninety'
+    else:
+        # If number in ones is 0, set to ten
+        if number[-1] == '0':
+            return 'ten'
+        # Check for teens (11 - 19)
+        if number[-1] == '1':
+            return 'eleven'
+        elif number[-1] == '2':
+            return 'twelve'
+        elif number[-1] == '3':
+            return 'thirteen'
+        elif number[-1] == '4':
+            return 'fourteen'
+        elif number[-1] == '5':
+            return 'fifteen'
+        elif number[-1] == '6':
+            return 'sixteen'
+        elif number[-1] == '7':
+            return 'seventeen'
+        elif number[-1] == '8':
+            return 'eighteen'
+        elif number[-1] == '9':
+            return 'ninteen'
+
+
+def hundreds(number):
+    if number == '0':
+        return ''
+    elif number == '1':
+        return 'one hundred'
+    elif number == '2':
+        return 'two hundred'
+    elif number == '3':
+        return 'three hundred'
+    elif number == '4':
+        return 'four hundred'
+    elif number == '5':
+        return 'five hundred'
+    elif number == '6':
+        return 'six hundred'
+    elif number == '7':
+        return 'seven hundred'
+    elif number == '8':
+        return 'eight hundred'
+    elif number == '9':
+        return 'nine hundred'
+
+
+def thousands(number):
+    if number == '0':
+        return 'thousand'
+    elif number == '1':
+        return 'one thousand'
+    elif number == '2':
+        return 'two thousand'
+    elif number == '3':
+        return 'three thousand'
+    elif number == '4':
+        return 'four thousand'
+    elif number == '5':
+        return 'five thousand'
+    elif number == '6':
+        return 'six thousand'
+    elif number == '7':
+        return 'seven thousand'
+    elif number == '8':
+        return 'eight thousand'
+    elif number == '9':
+        return 'nine thousand'
+
+
+def ten_thousands(number):
+    if number == '0':
+        return ''
+    elif number == '1':
+        return 'ten'
+    elif number == '2':
+        return 'twenty'
+    elif number == '3':
+        return 'thirty'
+    elif number == '4':
+        return 'forty'
+    elif number == '5':
+        return 'fifty'
+    elif number == '6':
+        return 'sixty'
+    elif number == '7':
+        return 'seventy'
+    elif number == '8':
+        return 'eighty'
+    elif number == '9':
+        return 'ninety'
+
+
+def spellNum(n):
+    n = str(n)
+    word_number = ""
+    num_digits = len(str(n))
+    # Single digit case
+    if num_digits == 1:
+            # If input is single digit
+            if word_number == '':
+                word_number = ones(n[-num_digits])
+                return word_number
+    while num_digits > 1:
+        if num_digits == 5:
+            word = ten_thousands(n[-num_digits])
+            word_number += word
+        elif num_digits == 4:
+            word = thousands(n[-num_digits])
+            word_number += word
+        elif num_digits == 3:
+            word = hundreds(n[-num_digits])
+            word_number += word
+        elif num_digits == 2:
+            # Double digits case
+            word = tens(n[-num_digits:])
+            word_number += word
+        num_digits -= 1
+    word = ones_mul(n[-num_digits])
+    return word_number
---
+++ number_speller_test.py
@@ -0,0 +1,33 @@
+import unittest
+import number_speller as ns
+
+class spellerTest(unittest.TestCase):
+    def test_singleDigit(self):
+        self.assertEqual(ns.spellNum(0), 'zero')
+        self.assertEqual(ns.spellNum(5), 'five')
+
+    def test_doubleDigit(self):
+        self.assertEqual(ns.spellNum(17), 'seventeen')
+        self.assertEqual(ns.spellNum(61), 'sixty one')
+
+    def tripleDigit(self):
+        self.assertEqual(ns.spellNum(400), 'four hundred')
+        self.assertEqual(ns.spellNum(304), 'three hundred four')
+        self.assertEqual(ns.spellNum(713), 'seven hundred thirteen')
+        self.assertEqual(ns.spellNum(192), 'one hundred ninety two')
+
+    def quadDigit(self):
+        self.assertEqual(ns.spellNum(2000), 'two thousand')
+        self.assertEqual(ns.spellNum(3008), 'three thousand eight')
+        self.assertEqual(ns.spellNum(6015), 'six thousand fifteen')
+        self.assertEqual(ns.spellNum(8387), 'eight thousand eighty seven')
+
+    def quinDigit(self):
+        self.assertEqual(ns.spellNum(70000), 'seventy thousand')
+        self.assertEqual(ns.spellNum(20002), 'twenty thousand two')
+        self.assertEqual(ns.spellNum(80019), 'eighty thousand ninteen')
+        self.assertEqual(ns.spellNum(83870), 'eighty three thousand eighty seventy')
+        self.assertEqual(ns.spellNum(51629), 'fifty one thousand six hundred nine')
+
+if __name__ == "__main__":
+    unittest.main()
